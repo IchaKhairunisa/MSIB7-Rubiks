93d491813405bd9ded7564ddcafe3615dda4594b
achmadfauzan-17 <achmadfauzan820@gmail.com>
2024-12-10 00:50:45 +0700

    Setup BE, Update FE, Add Routes, Controller, Middleware

---
+++ BE/routes/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { uploadFiles } from "../middlewares/UploadFile.js";
+import WorksController from "../controller/WorksController.js";
+
+const router = express.Router();
+
+router.post("/works", uploadFiles, WorksController.createWorks);
+router.get("/works", WorksController.getAllWorks);
+router.get("/works/:worksId", WorksController.getWorksById);
+router.put("/works/:worksId", uploadFiles, WorksController.updateWorksById);
+router.delete("/works/:worksId", WorksController.deleteWorksById);
+
+export default router;
